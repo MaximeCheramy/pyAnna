0a05ae5bf69a579f48967dfc7a495c21af5ed99b
Maxime Chéramy <maxime81@gmail.com>
2011-09-16 22:16:06 +0200

    Ajout GPLv3.

--- modules/talkative.py
+++ modules/talkative.py
@@ -1,4 +1,22 @@
 #encoding:UTF-8
+#
+# Copyright (C) 2011 - pyAnna
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of
+# the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details at
+# http://www.gnu.org/copyleft/gpl.html
+# 
+# You should have received a copy of the GNU General Public License 
+# along with this program; if not, see <http://www.gnu.org/licenses>.
+#
+
 import random, string
 from module import Module
 
