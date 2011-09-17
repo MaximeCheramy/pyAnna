2a7e76de3359ff3859e26874f280692ae88527e4
Maxime Chéramy <maxime81@gmail.com>
2011-09-17 13:11:32 +0200

    [module calc] Utilisation de eval, même si c'est beaucoup moins glorieux d'un coup.

--- modules/calc.py
+++ modules/calc.py
@@ -23,83 +23,16 @@ class Calc(Module):
 	def __init__(self, room):
 		self.room = room
 
-	def convert(self, expression):
-		operateurs = "+*-/x"
-		chiffres = "0123456789"
-		priorite = dict()
-		priorite['+'] = 1
-		priorite['-'] = 1
-		priorite['*'] = 2
-		priorite['x'] = 2
-		priorite['/'] = 2
-		s = []
-		ss = [s]
-		postfix = []
-		prev_was_chiffre = False 
-		for e in expression:
-			if e in operateurs:
-				while len(s) > 0:
-					t = s.pop()
-					if priorite[t] >= priorite[e]:
-						postfix.append(t)
-					else:
-						s.append(t)
-						break
-				s.append(e)
-				prev_was_chiffre = False
-			elif e in chiffres:
-				if prev_was_chiffre:
-					t = postfix.pop()
-					postfix.append(t * 10 + int(e))
-				else:
-					postfix.append(int(e))
-				prev_was_chiffre = True
-			elif e == '(':
-				s = []
-				ss.append(s)
-			elif e == ')':
-				while len(s) > 0:
-					postfix.append(s.pop())
-
-				ss.pop()
-				s = ss.pop()
-				ss.append(s)
-
-		while len(s) > 0:
-			postfix.append(s.pop())
-					
-		return postfix
-	
-	def calc(self, expression):
-		postfix = self.convert(expression)
-		s = []
-		for symbol in postfix:
-			if type(symbol) == int:
-				s.append(symbol)
-			else:
-				a2 = s.pop()
-				a1 = s.pop()
-				if symbol == '+':
-					r = a1 + a2
-				elif symbol == '-':
-					r = a1 - a2
-				elif symbol == '/':
-					r = a1 / a2
-				elif symbol == '*' or symbol == 'x':
-					r = a1 * a2
-				s.append(r)
-		return s.pop()
-
 	def handle_message(self, msg):
 		if msg['body'].startswith("!calc "):
 			try:
-				self.room.send_message(str(self.calc(msg['body'][6:])))
+				self.room.send_message(str(eval(msg['body'][6:])))
 			except:
 				self.room.send_message('yé pas compris.')
 
 	def handle_private_message(self, msg, to):
 		if msg['body'].startswith("!calc "):
 			try:
-				self.room.send_private_message(str(self.calc(msg['body'][6:])), to)
+				self.room.send_private_message(str(eval(msg['body'][6:])), to)
 			except:
-				self.room.send_message('yé pas compris.')
+				self.room.send_message_message('yé pas compris.')
